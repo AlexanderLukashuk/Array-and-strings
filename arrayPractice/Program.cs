7cb3c6d51ad6d20710139bc023ad1bd60253836b
Саня <sanya@Sergejs-MacBook-Air.local>
2020-06-03 15:53:27 +0600

    практическая работа и домашняя по теме массовов и строк

---
+++ arrayPractice/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+
+namespace arrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arrayA = new double[5];
+            double[,] arrayB = new double[3, 4];
+
+            for (int i = 0; i < arrayA.Length; i++)
+            {
+                Console.WriteLine("Enter number: ");
+                //double number = double.Parse();
+                arrayA[i] = double.Parse(Console.ReadLine());
+            }
+
+
+            Random rand = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    arrayB[i, j] = rand.Next();
+                }
+            }
+
+            for (int i = 0; i < arrayA.Length; i++)
+            {
+                Console.Write($"{arrayA[i]}");
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write($"{arrayB[i, j]}");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+
+            // №2
+            int[] arrayM = new int[10];
+            int[] arrayN = new int[8];
+            int size = 0;
+
+            for (int i = 0; i < arrayM.Length; i++)
+            {
+                arrayM[i] = rand.Next(0, 20);
+            }
+
+            for (int i = 0; i < arrayM.Length; i++)
+            {
+                Console.Write($"{arrayM[i]}");
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < arrayN.Length; i++)
+            {
+                arrayN[i] = rand.Next(0, 20);
+            }
+
+            for (int i = 0; i < arrayN.Length; i++)
+            {
+                Console.Write($"{arrayN[i]}");
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < arrayM.Length; i++)
+            {
+                for (int j = 0; j < arrayN.Length; j++)
+                {
+                    if (arrayN[j] == arrayM[i])
+                    {
+                        size++;
+                        break;
+                    }
+                }
+            }
+
+            int[] arrayOfCommonElements = new int[size];
+
+            int tmp = 0;
+            for (int i = 0; i < arrayM.Length; i++)
+            {
+                for (int j = 0; j < arrayN.Length; j++)
+                {
+                    if (arrayN[j] == arrayM[i])
+                    {
+                        arrayOfCommonElements[tmp] = arrayN[j];
+                        tmp++;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < arrayOfCommonElements.Length; i++)
+            {
+                Console.Write($"{arrayOfCommonElements[i]}");
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            // №3
+            string palindromString;
+            bool isPalindrom = false;
+            Console.WriteLine("Enter string: ");
+            palindromString = Console.ReadLine();
+            palindromString = palindromString.ToLower();
+
+            for (int i = 0; i < palindromString.Length / 2; i++)
+            {
+                if (palindromString.Length % 2 != 0)
+                {
+                    break;
+                }
+                else
+                {
+                    if (palindromString[i] == palindromString[palindromString.Length - i - 1])
+                    {
+                        isPalindrom = true;
+                    }
+                    else
+                    {
+                        isPalindrom = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPalindrom)
+            {
+                Console.WriteLine("It is palindrom");
+            }
+            else
+            {
+                Console.WriteLine("It is not palindrom");
+            }
+
+            // №4
+            string sString;
+            int count = 0;
+            Console.WriteLine("Enter string: ");
+            sString = Console.ReadLine();
+
+            for (int i = 0; i < sString.Length; i++)
+            {
+                if (sString[i] == ' ')
+                {
+                    count++;
+                }
+            }
+            count++;
+
+            Console.WriteLine($"Word count = {count}");
+
+            // №5
+            const int ARRAY_SIZE = 5;
+            int[,] array = new int[ARRAY_SIZE, ARRAY_SIZE];
+            int minElement = 0;
+            int maxElement = 0;
+            int minI = 0, maxI = 0;
+            int minJ = 0, maxJ = 0;
+            int amountOfElements = 0;
+
+            for (int i = 0; i < ARRAY_SIZE; i++)
+            {
+                for (int j = 0; j < ARRAY_SIZE; j++)
+                {
+                    array[i, j] = rand.Next(-100, 100);
+                }
+            }
+
+            for (int i = 0; i < ARRAY_SIZE; i++)
+            {
+                for (int j = 0; j < ARRAY_SIZE; j++)
+                {
+                    if (array[i, j] < minElement)
+                    {
+                        minElement = array[i, j];
+                        minI = i;
+                        minJ = j;
+                    }
+
+                    if (array[i, j] > maxElement)
+                    {
+                        maxElement = array[i, j];
+                        maxI = i;
+                        maxJ = j;
+                    }
+                }
+            }
+
+            for (int i = 0; i < ARRAY_SIZE; i++)
+            {
+                for (int j = 0; j < ARRAY_SIZE; j++)
+                {
+                    Console.Write($"{array[i, j]}");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+            if (minI < maxI)
+            {
+                for (int i = minI; i < maxI; i++)
+                {
+                    for (int j = 0; j < ARRAY_SIZE; j++)
+                    {
+                        amountOfElements += array[i, j];
+                    }
+                }
+            }
+            else
+            {
+                for (int i = maxI; i < minI; i++)
+                {
+                    for (int j = 0; j < ARRAY_SIZE; j++)
+                    {
+                        amountOfElements += array[i, j];
+                    }
+                }
+            }
+
+            for (int j = 0; j < minJ; j++)
+            {
+                amountOfElements -= array[minI, j];
+            }
+
+            Console.WriteLine($"The amount of elements = {amountOfElements}");
+
+        }
+
+
+    }
+}
---
+++ homework/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // №1
+            char[] stringWithoutDot = new char[10000];
+            char tmp;
+            string symbol;
+            int countOfSpace = 0;
+            int counter = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Enter symbol: ");
+                symbol = Console.ReadLine();
+                if (symbol == ".")
+                {
+                    break;
+                }
+
+                if (symbol == " ")
+                {
+                    countOfSpace++;
+                }
+                stringWithoutDot[counter] = char.Parse(symbol);
+                counter++;
+            }
+
+            Console.WriteLine($"Count of space = {countOfSpace}");
+
+            // №2
+            string input;
+            int luckyTicket;
+            int firstThreeDigits = 0;
+            int lastThreeDigits = 0;
+            int tmp1, tmp2;
+
+            while (true)
+            {
+                Console.Write("Enter 6 digit number: ");
+                input = Console.ReadLine();
+                luckyTicket = int.Parse(input);
+
+                if (luckyTicket / 100000 > 0 && luckyTicket / 100000 < 10)
+                {
+                    break;
+                }
+            }
+
+            tmp1 = luckyTicket % 10;
+            lastThreeDigits += tmp1;
+            tmp2 = (luckyTicket % 100) / 10;
+            lastThreeDigits += tmp2;
+            lastThreeDigits += (luckyTicket % 1000) / 100;
+
+            firstThreeDigits += luckyTicket / 100000;
+            firstThreeDigits += (luckyTicket / 10000) % 10;
+            firstThreeDigits += (luckyTicket / 1000) % 10;
+
+            //Console.WriteLine($"{luckyTicket}");
+            //Console.WriteLine($"{lastThreeDigits}");
+            //Console.WriteLine($"{firstThreeDigits}");
+
+            if (firstThreeDigits == lastThreeDigits)
+            {
+                Console.WriteLine("It is a lucky ticket");
+            }
+            else
+            {
+                Console.WriteLine("It is not a lucky ticket");
+            }
+
+            // №3
+            char[] transferToAnotherRegister = new char[10000];
+            int tmpSymbol;
+            counter = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Enter '.' to finish");
+                Console.WriteLine("Enter symbol: ");
+                tmpSymbol = Console.Read();
+                //Console.WriteLine();
+
+                if (tmpSymbol >= 65 && tmpSymbol <= 90)
+                {
+                    tmpSymbol += 32;
+                    transferToAnotherRegister[counter] = (char)tmpSymbol;
+                    counter++;
+                }
+                else if (tmpSymbol >= 97 && tmpSymbol <= 122)
+                {
+                    tmpSymbol -= 32;
+                    transferToAnotherRegister[counter] = (char)tmpSymbol;
+                    counter++;
+                }
+                else if (tmpSymbol == 46)
+                {
+                    break;
+                }
+            }
+
+            for (int i = 0; i < transferToAnotherRegister.Length; i++)
+            {
+                Console.Write($"{transferToAnotherRegister[i]}");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+            // №4
+            int numberA, numberB;
+            Console.Write("Enter number A: ");
+            input = Console.ReadLine();
+            numberA = int.Parse(input);
+
+            Console.Write("Enter number B: ");
+            input = Console.ReadLine();
+            numberB = int.Parse(input);
+
+            if (numberA < numberB)
+            {
+                for (int i = numberA; i <= numberB; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write($"{i}");
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                for (int i = numberB; i <= numberA; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write($"{i}");
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            // №5
+            int numberN;
+            Console.Write("Enter number N(>0): ");
+            input = Console.ReadLine();
+            numberN = int.Parse(input);
+
+            int tmpNumber1 = -1;
+            int tmpNumber2 = numberN;
+            numberN = 0;
+
+            while (tmpNumber1 != 0)
+            {
+                tmpNumber1 = tmpNumber2 % 10;
+                numberN += tmpNumber1;
+                numberN *= 10;
+                tmpNumber2 /= 10;
+            }
+            numberN /= 10;
+            numberN /= 10;
+
+            Console.WriteLine($"New Number N = {numberN}");
+
+        }
+    }
+}
